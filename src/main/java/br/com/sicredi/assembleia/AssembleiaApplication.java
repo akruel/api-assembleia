e18a1351d93628994524813be9c64b828b721426
Anderson Kruel <anderson.kruel@gmail.com.br>
2020-04-07 22:21:59 -0300

    commit inicial do projeto

---
+++ src/main/java/br/com/sicredi/assembleia/AssembleiaApplication.java
@@ -0,0 +1,17 @@
+package br.com.sicredi.assembleia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Trello do Projeto: https://trello.com/b/LLuq5hxp/api-assembleia
+ */
+
+@SpringBootApplication
+public class AssembleiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaApplication.class, args);
+	}
+
+}
