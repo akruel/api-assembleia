8b6a218e75b7802d0db582f3f0d3928fefa978a4
Anderson Kruel <anderson.kruel@gmail.com.br>
2020-04-08 01:19:22 -0300

    habilitando swagger

--- src/main/java/br/com/sicredi/assembleia/AssembleiaApplication.java
+++ src/main/java/br/com/sicredi/assembleia/AssembleiaApplication.java
@@ -4,12 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Trello do Projeto: https://trello.com/b/LLuq5hxp/api-assembleia
  */
 
+@EnableSwagger2
 @SpringBootApplication
 public class AssembleiaApplication {
 
@@ -21,5 +27,14 @@ public class AssembleiaApplication {
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
 	}
+
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+          .select()
+          .apis(RequestHandlerSelectors.any())
+          .paths(PathSelectors.any())
+          .build();
+    }
 
 }
